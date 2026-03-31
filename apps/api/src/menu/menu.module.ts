c9b73e54d5f1c56edd28dcffb70ecdeb52f8be92
Rashmika Madaela <rashmikamadaelaofficial@gmail.com>
2026-03-31 21:49:19 +0530

    imported AuthModule

--- apps/api/src/menu/menu.module.ts
+++ apps/api/src/menu/menu.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MenuController } from './menu.controller';
 import { MenuService } from './menu.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [MenuController],
   providers: [MenuService],
 })
